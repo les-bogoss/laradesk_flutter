336ed8819fbac6f1cc0765c1dc9393ef0e033b8a
Célian HAMON <skelletondude@gmail.com>
2022-07-21 10:12:32 +0200

    🙀 created ticket content page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,14 @@
 import 'package:laradesk_flutter/views/home/home.dart';
+import 'package:flutter/material.dart';
 import 'package:laradesk_flutter/views/tickets/tickets.dart';
-
 import 'views/home/login.dart';
 import 'views/home/register.dart';
+import 'views/tickets/ticket.dart';
 
 var appRoutes = {
   '/': (context) => const HomePage(),
   '/login': (context) => const LoginPage(),
   '/register': (context) => const RegisterPage(),
   '/tickets': (context) => const TicketList(),
+  '/ticket': (context) => const TicketPage(),
 };
