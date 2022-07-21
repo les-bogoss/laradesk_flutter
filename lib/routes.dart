092a48117b914b95eeec6fd50501bf330a3f91bc
HAMON Célian <skelletondude@gmail.com>
2022-07-21 10:13:36 +0200

    Revert "🙀 created ticket content page"

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,9 @@
 import 'package:laradesk_flutter/views/home/home.dart';
-import 'package:flutter/material.dart';
 import 'package:laradesk_flutter/views/tickets/tickets.dart';
 import 'package:laradesk_flutter/views/utils/navbar.dart';
+
 import 'views/home/login.dart';
 import 'views/home/register.dart';
-import 'views/tickets/ticket.dart';
 
 var appRoutes = {
   '/': (context) => const NavBar(
@@ -14,5 +13,4 @@ var appRoutes = {
   '/login': (context) => const LoginPage(),
   '/register': (context) => const RegisterPage(),
   '/tickets': (context) => const TicketList(),
-  '/ticket': (context) => const TicketPage(),
 };
