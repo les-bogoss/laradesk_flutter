14f33053f75286d80dfc6d2a825fdbc95360b561
p2acoo <78920246+p2acoo@users.noreply.github.com>
2022-07-22 14:01:52 +0200

    remove debug

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
