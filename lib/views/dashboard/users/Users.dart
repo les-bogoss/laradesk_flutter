918a36e3da005e68edd73dc02dd83d2ff7977de6
alancesi <alan.courtois@viacesi.fr>
2022-07-22 09:49:06 +0200

    🏗 Added Users in dashboard

---
+++ lib/views/dashboard/users/Users.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../../controllers/get_all_users.dart';
+
+class UsersList extends StatefulWidget {
+  const UsersList({Key? key}) : super(key: key);
+
+  @override
+  State<UsersList> createState() => _UsersListState();
+}
+
+class _UsersListState extends State<UsersList> {
+  //get all tickets from server
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future getUsers() async {
+    //get all tickets from server
+    var users = await getAllUsers();
+    return users;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Users | Dashboard',
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF094074),
+        elevation: 0,
+      ),
+      //list view all tickets
+      body: FutureBuilder(
+        future: getAllUsers(),
+        initialData: const [],
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data.isEmpty) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {
+                    //go to ticket detail
+                    Navigator.pushNamed(
+                      context,
+                      '/user',
+                      arguments: {'id': snapshot.data[index]['id'].toString()},
+                    );
+                  },
+                  child: ListTile(
+                    title: Text(
+                        snapshot.data[index]['last_name'].toUpperCase() +
+                            ' ' +
+                            snapshot.data[index]['first_name']),
+                    subtitle: Text(snapshot.data[index]['id'].toString()),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      backgroundImage: NetworkImage(
+                          "https://34.140.17.43${snapshot.data[index]['avatar']}"),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/views/dashboard/users/user.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:laradesk_flutter/controllers/get_user.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  final myTitle = TextEditingController();
+
+  File? _image;
+  PickedFile? _pickedFile;
+  final _picker = ImagePicker();
+
+  Future<void> _getImage() async {
+    _pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    if (_pickedFile != null) {
+      setState(() {
+        _image = File(_pickedFile!.path);
+      });
+    } else {
+      print('image: $_image');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+
+    if (args['id'] == null) {
+      return const Center(
+        child: Text('No ticket id provided'),
+      );
+    } else {
+      final user = getUser(args['id'] ?? '');
+
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color(0xFF094074),
+          elevation: 0,
+          title: Text(
+            "User - ${args['id']}",
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            FutureBuilder(
+                builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data!['id'] != null) {
+                      return Center(
+                        child: Column(
+                          children: [
+                            const Padding(padding: EdgeInsets.all(10)),
+                            CircleAvatar(
+                              backgroundColor: Colors.blue,
+                              backgroundImage: NetworkImage(
+                                  "https://34.140.17.43${snapshot.data!['avatar']}"),
+                              radius: 100,
+                            ),
+                            const Padding(padding: EdgeInsets.all(10)),
+                            Text(
+                                "Name : ${snapshot.data!['last_name'].toUpperCase()} ${snapshot.data!['first_name']}"),
+                            Text("Email : ${snapshot.data!['email']}"),
+                            const Padding(padding: EdgeInsets.all(10)),
+                            Text(
+                                "Created date : ${DateFormat('dd MMMM yyyy').format(DateTime.parse(snapshot.data!['created_at']))}"),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return const Center(
+                        child: Text('No User found'),
+                      );
+                    }
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+                future: user),
+          ],
+        ),
+      );
+    }
+  }
+}
+
+Widget ticketContentTile(
+    String text, String date, String author, String avatar, context) {
+  return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(10),
+      child: InkWell(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.blue,
+              backgroundImage: NetworkImage("https://34.140.17.43$avatar"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '$author - $date',
+                    style: const TextStyle(fontSize: 15.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.5),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        text.replaceAll('<br />', '\n'),
+                        style: const TextStyle(fontSize: 13.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        onTap: () {},
+      ));
+}
